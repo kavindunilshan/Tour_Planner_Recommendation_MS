a85530c2f8032daed8bd5569528e73f5d48adefd
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-09-27 23:34:16 +0530

    Initialization is done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 
-const routes = require("./routes")
+const routes = require("./routes/hotels")
 app.use("/api", routes)
 app.use(cors());
 
