37026ed6b2ff1d761aa1c6ebcf2993d20590eef2
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-10-06 23:37:03 +0530

    3 source functions created for external api calls

--- source/hotelSource.js
+++ source/hotelSource.js
@@ -1,43 +1,43 @@
 const axios = require("axios");
 // 7.2906
 // 80.6337
-const options = {
-  method: 'GET',
-  url: 'https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates',
-  params: {
-    units: 'metric',
-    room_number: '1',
-    longitude: '79.8612',
-    latitude: '6.9271',
-    // longitude: '80.6337',
-    // latitude: '7.2906',
-    filter_by_currency: 'AED',
-    order_by: 'class_descending',
-    locale: 'en-gb',
-    checkout_date: '2023-09-28',
-    adults_number: '2',
-    checkin_date: '2023-09-27',
-    children_ages: '5,0',
-    include_adjacency: 'true',
-    children_number: '2',
-    page_number: '0',
-    categories_filter_ids: 'class::2,class::4,free_cancellation::1'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'eb62a4e22cmsh6fa4648e0b05b79p1e8afdjsnfdac33378d64',
-    'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
-  }
-};
 
 
-const getHotels = async () => {
+
+const getHotels = async (longitude, latitude) => {
+    const options = {
+        method: 'GET',
+        url: 'https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates',
+        params: {
+            units: 'metric',
+            room_number: '1',
+            longitude: longitude,
+            latitude: latitude,
+            filter_by_currency: 'AED',
+            order_by: 'class_descending',
+            locale: 'en-gb',
+            checkout_date: '2023-09-30',
+            adults_number: '2',
+            checkin_date: '2023-09-29',
+            children_ages: '5,0',
+            include_adjacency: 'true',
+            children_number: '2',
+            page_number: '0',
+            categories_filter_ids: 'class::2,class::4,free_cancellation::1'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'eb62a4e22cmsh6fa4648e0b05b79p1e8afdjsnfdac33378d64',
+            'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+        }
+    };
+
     try {
-	const response = await axios.request(options);
-	console.log(response.data);
-    return response.data.result;
-} catch (error) {
-	console.error(error);
-}
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data.result;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = getHotels;
